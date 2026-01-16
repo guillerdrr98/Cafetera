bc1b22c39323a458a6ea782de84e18f32492bf22
guillerdrr98 <guillerdrr98@educastur.es>
2026-01-16 11:04:36 +0100

    Creado proyecto Cafetera

---
+++ wp/Cafetera/src/cafetera/recursos/Cafetera.java
@@ -0,0 +1,5 @@
+package cafetera.recursos;
+
+public class Cafetera {
+
+}
---
+++ wp/Cafetera/src/cafetera/tests/CafeteraTests.java
@@ -0,0 +1,14 @@
+package cafetera.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CafeteraTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
