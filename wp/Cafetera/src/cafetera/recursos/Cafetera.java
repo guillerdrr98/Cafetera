b4257c26ba68c71f13fd6f6954243a2126c14b3a
guillerdrr98 <guillerdrr98@educastur.es>
2026-01-21 10:50:21 +0100

    Se ha completado la clase Cafetera y se han implementado varios tests

--- wp/Cafetera/src/cafetera/recursos/Cafetera.java
+++ wp/Cafetera/src/cafetera/recursos/Cafetera.java
@@ -2,6 +2,77 @@ package cafetera.recursos;
 
 public class Cafetera {
 
-	private int capacidad;
+	private int cantidadActual;
 	private int capacidadMaxima;
+	
+	public Cafetera() {
+		capacidadMaxima = 1000;
+		cantidadActual = 0;
+	}
+	
+	public Cafetera(int capacidad) {
+		capacidadMaxima = capacidad;
+		cantidadActual = capacidadMaxima;
+	}
+	
+	public Cafetera(int capacidad, int cantidad) {
+		this.capacidadMaxima = capacidad;
+		this.cantidadActual = cantidad;
+	}
+	
+	public int getCapacidadMaxima() {
+		return this.capacidadMaxima;
+	}
+	
+	public int getCantidadActual() {
+		return this.cantidadActual;
+	}
+	
+	public void setCapacidadMaxima(int c) {
+		capacidadMaxima = c;
+	}
+	
+	public void setCantidadActual(int c) {
+		cantidadActual = c;
+	}
+	
+	public void llenarCafetera() {
+		cantidadActual = capacidadMaxima;
+	}
+	
+	public int servirTaza(int cc) {
+	    int servido = 0;
+	    if (cantidadActual == 0) {
+	        System.out.println("Lo siento, pero no queda nada de café.");
+	        servido = 0;
+	    }
+	    else {
+	        if (cantidadActual <= cc) {
+	            System.out.println("Se ha servido todo lo que quedaba (" +
+	                               cantidadActual + " c.c.)");
+	            servido = cantidadActual;
+	            cantidadActual = 0;
+	        }
+	        else {
+	            servido = cc;
+	            cantidadActual -= cc;
+	            System.out.println("Todavía quedan " + cantidadActual + " c.c.");
+	        }
+	    }
+	    return servido;
+	}
+
+	public void vaciarCafetera() {
+	    cantidadActual = 0;
+	}
+
+	public void agregarCafe(int cc) {
+	    cantidadActual += cc;
+	}
+
+	public String toString() {
+	    return "Capacidad = " + capacidadMaxima +
+	           " Cantidad actual = " + cantidadActual;
+	}
+	
 }
--- wp/Cafetera/src/cafetera/tests/CafeteraTests.java
+++ wp/Cafetera/src/cafetera/tests/CafeteraTests.java
@@ -4,11 +4,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cafetera.recursos.Cafetera;
+
 class CafeteraTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor1() {
+		Cafetera c = new Cafetera();
+		assertEquals(1000, c.getCapacidadMaxima());
+		assertEquals(0, c.getCantidadActual());
+	}
+	
+	@Test
+	void testConstructor2() {
+		int capacidad = 3000;
+		Cafetera c = new Cafetera(capacidad);
+		assertEquals(capacidad, c.getCapacidadMaxima());
+		assertEquals(c.getCapacidadMaxima(), c.getCantidadActual());
+	}
+	
+	@Test
+	void testConstructor3() {
+		int capacidad = 3000;
+		int cantidad = 400;
+		Cafetera c = new Cafetera(capacidad, cantidad);
+		assertEquals(capacidad, c.getCapacidadMaxima());
+		assertEquals(cantidad, c.getCantidadActual());
+	}
+	
+	@Test
+	void testMetodoSetYGetCapacidad() {
+		int capacidadEsperada = 2500;
+		Cafetera c = new Cafetera();
+		c.setCapacidadMaxima(capacidadEsperada);
+		assertEquals(capacidadEsperada, c.getCapacidadMaxima());
+	}
+	
+	@Test
+	void testMetodoSetYGetCantidad() {
+		int cantidadEsperada = 400;
+		Cafetera c = new Cafetera();
+		c.setCantidadActual(cantidadEsperada);
+		assertEquals(cantidadEsperada, c.getCantidadActual());
+	}
+	
+	@Test
+	void testLlenarCafetera() {
+		Cafetera c = new Cafetera();
+		assertEquals(0,c.getCantidadActual());
+		c.llenarCafetera();
+		assertEquals(c.getCapacidadMaxima(), c.getCantidadActual());
+	}
+	
+	@Test
+	void testVaciarCafetera() {
+		int capacidadMaxima = 2000;
+		Cafetera c = new Cafetera(capacidadMaxima, capacidadMaxima);
+		assertEquals(c.getCapacidadMaxima(), c.getCantidadActual());
+		c.vaciarCafetera();
+		assertEquals(0,c.getCantidadActual());
+	}
+	
+	@Test
+	void testAgregarCafe() {
+		int cantidadEsperada = 800;
+		int cantidadInicial = 500;
+		int capacidadMaxima = 2000;
+		Cafetera c = new Cafetera(capacidadMaxima, cantidadInicial);
+		assertEquals(500, c.getCantidadActual());
+		assertEquals(2000, c.getCapacidadMaxima());
+		c.agregarCafe(300);
+		assertEquals(cantidadEsperada, c.getCantidadActual());
 	}
 
 }
